cb4aefe000e6477a5f00d64d810a76490e1b0b84
test <xinzon32177@163.com>
2022-04-18 12:47:46 +0800

    feat(实现): 完成

---
+++ src/Cache.ts
@@ -0,0 +1,45 @@
+import { CustomConfig } from './types';
+
+type KeyHandler<K> = (k: K) => string;
+
+export default class Cache<K, V> {
+  private readonly cache = new Map<string, { value: V; expires: number }>();
+
+  private readonly keyHandler: KeyHandler<K>;
+
+  constructor(keyHandler?: KeyHandler<K>) {
+    this.keyHandler = keyHandler || ((k) => JSON.stringify(k));
+  }
+
+  clearDeadCache() {
+    const now = Date.now();
+    for (const [k, v] of this.cache.entries()) {
+      if (v.expires > now) continue;
+      this.cache.delete(k);
+    }
+  }
+
+  get(key: K) {
+    const k = this.keyHandler(key);
+    const v = this.cache.get(k);
+    if (!v) return null;
+    const now = Date.now();
+    if (now > v.expires) {
+      this.cache.delete(k);
+      return null;
+    }
+    return v.value;
+  }
+
+  set(key: K, value: V, customConfig: CustomConfig = {}) {
+    const defaultTimeout = 5 * 1000;
+    const timeout =
+      typeof customConfig.useCache === 'object' ? customConfig.useCache?.timeout : defaultTimeout;
+    this.cache.set(this.keyHandler(key), { value, expires: timeout + Date.now() });
+    this.clearDeadCache();
+  }
+
+  has(key: K): boolean {
+    return this.get(key) !== null;
+  }
+}
