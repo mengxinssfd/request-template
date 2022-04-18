b70bc390fa75e67c104c22edda95de05648f1d76
test <xinzon32177@163.com>
2022-04-18 12:38:48 +0800

    feat(init): 完成

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /* 优先级低于parse的语法解析配置 */
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // typescript-eslint推荐规则
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    // 禁止使用 var
+    'no-var': 'error',
+    // 不使用结尾分号
+    semi: 'off',
+    // 优先使用 interface 而不是 type
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
