4d2c7b81f5ab87a400beffcd63ffae88e402e052
dyh_a <xinzon32177@163.com>
2022-08-16 15:29:32 +0800

    test(AxiosRequestTemplate.ts): deleteCacheByTag

--- __test__/cacheConfig.test.ts
+++ __test__/cacheConfig.test.ts
@@ -182,4 +182,35 @@ describe('mock cacheConfig', () => {
     const res5 = await req.request({ url: '/data', data: { a: 5 } }, { cache: { timeout: 20000 } });
     expect(res5).toEqual({ a: 3 });
   });
+  test('deleteCacheByTag', async () => {
+    const req = new AxiosRequestTemplate<CustomConfig>();
+    (req as any).generateRequestKey = function (ctx) {
+      return ctx.requestConfig.url;
+    };
+    const tag = Symbol('tag');
+    const res = await req.request(
+      { url: '/data', data: { a: 1 } },
+      { cache: { timeout: 20000 }, tag },
+    );
+    expect(res).toEqual({ a: 1 });
+
+    // 复用1
+    const res2 = await req.request(
+      { url: '/data', data: { a: 2 } },
+      { cache: { timeout: 20000 }, tag },
+    );
+    expect(res2).toBe(res);
+
+    req.deleteCacheByTag(tag);
+    const res3 = await req.request({ url: '/data', data: { a: 3 } }, { cache: { timeout: 20000 } });
+    expect(res3).toEqual({ a: 3 });
+
+    // 复用3
+    const res4 = await req.request({ url: '/data', data: { a: 4 } }, { cache: { timeout: 20000 } });
+    expect(res4).toEqual({ a: 3 });
+
+    // 复用3
+    const res5 = await req.request({ url: '/data', data: { a: 5 } }, { cache: { timeout: 20000 } });
+    expect(res5).toEqual({ a: 3 });
+  });
 });
