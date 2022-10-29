15bd965280498de4ceb8aed616b8f1adc8bba5d8
dyh_a <xinzon32177@163.com>
2022-10-29 16:17:13 +0800

    feat: vue3-hooks

---
+++ packages/vue3-hooks/src/types.ts
@@ -0,0 +1,15 @@
+export type FN = (...args: any[]) => Promise<any>;
+
+export interface State<T extends FN, D, TD = Awaited<ReturnType<T>>['data']> {
+  loading: boolean;
+  data: D extends TD ? TD : TD | null;
+  error: any | null;
+}
+
+export type Options<A extends string, D extends object | void = void> = D extends void
+  ? { requestAlias?: A; immediate?: boolean }
+  : {
+      immediate?: boolean;
+      data?: D;
+      dataDriver?: boolean;
+    };
