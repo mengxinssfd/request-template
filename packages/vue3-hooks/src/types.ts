ac2e160e4985834af1cf3112f92f1937615f5e4f
dyh_a <xinzon32177@163.com>
2023-05-14 09:41:32 +0800

    docs(vue3-hooks): 添加注释

--- packages/vue3-hooks/src/types.ts
+++ packages/vue3-hooks/src/types.ts
@@ -14,18 +14,32 @@ export interface State<T extends FN, D, TD = Awaited<ReturnType<T>>['data']> {
 
 export interface CommonOptions {
   loading?: {
-    // 立即把loading设置为true
+    /**
+     * 立即把loading设置为true
+     */
     immediate?: boolean;
-    // 毫秒值；loading必须显示的时间
+    /**
+     * 毫秒值；loading必须显示的时间，设置以后返回的loading与data不再同步
+     */
     threshold?: number;
   };
 }
 
 export interface AliasOptions<A> extends CommonOptions {
+  // 解构赋值的时候是可以改名的，这里好像有点多余了，再观察看看
+  /**
+   * 请求函数别名
+   */
   requestAlias?: A;
 }
 export interface DataDriverOptions<D> extends CommonOptions {
+  /**
+   * 数据驱动时立即执行请求
+   */
   immediate?: boolean;
+  /**
+   * 数据，必须是vue3的响应式数据才可以数据驱动
+   */
   data: D;
 }
 
